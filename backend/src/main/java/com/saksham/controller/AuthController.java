a0312af31d756e4c21bf3d2397c10427eb4902ef
Atharv Kolekar <kolekar.atharv1812@gmail.com>
2026-02-23 23:20:50 +0530

    In Auth Section Implemented update feature for user that will update only passowrd, academicYear and phone

--- backend/src/main/java/com/saksham/controller/AuthController.java
+++ backend/src/main/java/com/saksham/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.saksham.controller;
 import com.saksham.dto.*;
 import com.saksham.service.AuthService;
 import lombok.RequiredArgsConstructor;
+
+import org.hibernate.sql.Update;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,6 +22,11 @@ public class AuthController {
     @PostMapping("/login")
     public AuthResponse login(@RequestBody LoginRequest request){
         return authService.login(request);
+    }
+
+    @PutMapping("/update")
+    public String updateUser(@RequestBody UpdateUserRequest request){
+        return authService.updateUser(request);
     }
     
 }
