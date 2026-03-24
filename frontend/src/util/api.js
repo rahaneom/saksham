36aea8e6c3ea03e4ebee5b1f053f2021e1e7ddde
Harshvardhan <harshvardhanwork01@gmail.com>
2026-03-24 10:03:56 +0530

    updated frontend with backend deployed urls

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://saksham-backend-fcd5.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
