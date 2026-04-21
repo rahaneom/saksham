192e1dfc0cdb32507f7f955096114f5af8aaca0c
Atharv Kolekar <126802492+atharvkolekar190@users.noreply.github.com>
2026-04-21 14:45:48 +0530

    Update API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://saksham-backend-fcd5.onrender.com",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://saksham-backend-fcd5.onrender.com",
+  // baseURL: "http://localhost:8080",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
